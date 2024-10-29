83deb208cfebd88fec1c31bec52f79439cc2772d
agustin mateos <agustinrotondomateos@gmail.com>
2024-10-29 19:33:13 -0300

    sempen parallax

--- src/components/PositiveImpact.jsx
+++ src/components/PositiveImpact.jsx
@@ -19,35 +19,34 @@ export default function AboutUs() {
         };
     }, []);
 
-    const startingPosition = -250; 
+    const startingPosition = -250;
 
     return (
-        <div className="min-h-screen md:min-h-[696px] relative overflow-hidden">
-          
+        <div className="relative overflow-hidden h-[50vh]  md:min-h-[796px]">
             <div
-                className="w-full bg-cover bg-center min-h-screen md:min-h-[696px] flex justify-between flex-col items-center transition-transform duration-1000 ease-out relative"
+                className="w-full bg-cover bg-center h-full flex flex-col items-center justify-between transition-transform duration-1000 ease-out relative"
                 style={{
-                    backgroundImage: ` url('/OurMissionImg.svg')`
+                    backgroundImage: `url('/OurMissionImg.svg')`,
                 }}
             >
-                <div className="h-[225px] text-[#FFFFFF] w-[944px] flex items-center text-center text-[32px] leading-[40px] font-medium tracking-widest">
+                {/* Contenedor de texto optimizado para móviles */}
+                <div className="text-center text-white w-[90%] max-w-[821px] sm:w-[80%] md:w-[60%] lg:w-[944px] flex items-center justify-center text-sm sm:text-base md:text-2xl lg:text-[32px] leading-tight font-medium tracking-widest p-2 md:h-[225px]">
                     <h4>
                         {t('OurMissionText')}
                     </h4>
                 </div>
 
-             
+                {/* Imagen Parallax a pantalla completa */}
                 <Image
                     src="/sempenparallax.svg"
                     alt="Parallax Image"
+                    layout="responsive"
                     width={1587}
                     height={60}
                     unoptimized={true}
-                    className="absolute"
+                    className="absolute bottom-0 left-0 w-full h-auto"
                     style={{
-                        bottom: `${startingPosition + scrollY * 0.2}px`, 
-                        left: '50%',
-                        transform: 'translateX(-50%)',
+                        bottom: `${startingPosition + scrollY * 0.2}px`,
                         transition: 'bottom 1s ease-out',
                     }}
                 />
