268bb0fd85ea2c21621b9196ea5e50aba5fb03ce
agustin mateos <agustinrotondomateos@gmail.com>
2024-11-11 18:51:52 -0300

    saf

--- src/components/GreenEnergy.jsx
+++ src/components/GreenEnergy.jsx
@@ -43,7 +43,7 @@ export default function GreenEnergy() {
     <div className="w-full bg-[#EDEDED] min-h-screen flex flex-col items-center ">
       <div className="flex justify-center  w-[332px] md:w-[700px] sm:w-[600px] lg:w-[950px] xl:w-[1212px] rounded-tl-[8px]">
         <div className="flex flex-col items-center">
-          <div className="flex space-x-4 mb-4 w-full max-w-[1218px] px-4">
+          <div className="flex space-x-4 mb-4 w-full max-w-[1218px] ">
             {Object.keys(buttonData).map((button) => (
               <button
                 key={button}
