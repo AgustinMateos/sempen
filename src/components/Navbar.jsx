3c7eea447b2cee0638d9c9bba41c5139ab831ef8
agustin mateos <agustinrotondomateos@gmail.com>
2024-11-26 15:15:16 -0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
                         key={link}
                         href={`#${link}`} 
                         className={`navbar-link text-center font-jakarta transition-all duration-500 ${
-                            index === 0 || index === 1 ? 'lg:w-[150px]' : 
+                            index === 0 || index === 1 ? 'lg:w-[150px] xl:w-[110px]' : 
                             index === 3 ? 'lg:w-[180px] xl:w-[140px]' : 'lg:w-[250px] xl:w-[210px]'
                         } ${activeLink === link ? 'text-[#57B6B2]' : 'text-white'}`}
                         onClick={() => handleLinkClick(link)}
