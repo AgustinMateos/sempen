e7e1d953af7d3f9277863871d79d9c659c667ed4
agustin mateos <agustinrotondomateos@gmail.com>
2024-12-03 13:48:15 -0300

    keywords

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -1,67 +1,54 @@
 import './globals.css';
 import { Archivo } from 'next/font/google';
 import { dir } from 'i18next';
-import Head from 'next/head';
 
 const archivo = Archivo({ subsets: ['latin'] });
 
-// Definir metadata globalmente, ya que lo necesitas en generateMetadata y en el componente
+// Definir metadata globalmente
 const metadata = {
-  title: 'Sempen',
+  title: {
+    en: 'Sempen - Fueling the future',
+    es: 'Sempen - Impulsando el futuro',
+    pt: 'Sempen - Impulsionando o futuro',
+  },
   description: {
-    en: 'Fueling the future',
-    es: 'Impulsando el futuro',
-    pt: 'Impulsionando o futuro',
+    en: 'We develop and build projects that produce sustainable fuels, enabling net-zero transportation and food production.Our goal is to drive down the "green premium".',
+    es: 'En Sempen desarrollamos y construimos proyectos que producen combustibles sostenibles, logrando el transporte y la producción de alimentos con emisiones netas cero. Nuestro objetivo es reducir la "prima verde".',
+    pt: 'Desenvolvemos e construímos projetos que produzem combustíveis sustentáveis, viabilizando emissões zero no setor de transportes e na cadeia de produção de alimentos. Nosso objetivo é reduzir o "prêmio verde".',
   },
   keywords: {
-    en: 'sustainable aviation fuels, SAF, green premium, flight plan green, sempen',
-    es: 'combustible sostenible de aviación, SAF, prima verde, sempen, plan de vuelo verde',
-    pt: 'plano de voo verde, sempen, saf, prêmio verde, combustíveis de aviação sustentáveis',
+    en: 'sustainable aviation fuels, SAF, green premium, eco-friendly aviation, carbon-neutral flight, renewable jet fuel, flight plan green, Sempen, sustainable travel solutions, alternative aviation fuels, biojet fuels, aviation decarbonization',
+    es: 'combustible sostenible de aviación, SAF, prima verde, aviación ecológica, vuelo carbono neutral, combustible renovable para aviones, plan de vuelo verde, Sempen, soluciones de viaje sostenible, combustibles alternativos para aviones, biocombustibles, descarbonización de la aviación',
+    pt: 'combustíveis de aviação sustentáveis, SAF, prêmio verde, aviação ecológica, voo carbono neutro, combustível renovável para jatos, plano de voo verde, Sempen, soluções de viagem sustentável, combustíveis alternativos de aviação, biocombustíveis para aviação, descarbonização da aviação',
   },
 };
 
 export async function generateMetadata({ params: { locale } }) {
-  const description = metadata.description[locale] || metadata.description.en;
-  const keywords = metadata.keywords[locale] || metadata.keywords.en;
-
   return {
-    title: metadata.title,
-    description: description,
-    keywords: keywords,
+    title: metadata.title[locale] || metadata.title.en,
+    description: metadata.description[locale] || metadata.description.en,
+    keywords: metadata.keywords[locale] || metadata.keywords.en,
+    openGraph: {
+      title: metadata.title[locale] || metadata.title.en,
+      description: metadata.description[locale] || metadata.description.en,
+      url: `https://sempen.com/${locale}`,
+      images: [
+        {
+          url: 'https://sempen.com/images/imagen.jpg',
+          alt: 'Sempen image',
+        },
+      ],
+    },
+    alternates: {
+      canonical: `https://sempen.com/${locale}`,
+    },
   };
 }
 
 export default function RootLayout({ children, params: { locale } }) {
-  // Obtener la descripción y las keywords para la página actual
-  const description = metadata.description[locale] || metadata.description.en;
-  const keywords = metadata.keywords[locale] || metadata.keywords.en;
-
-  // Definir la URL canónica para cada idioma
-  const canonicalUrl = `https://sempen.com/${locale}`;
-
-  <link rel="icon" href="/fav1.jpg" />
   return (
     <html lang={locale} dir={dir(locale)}>
-      <Head>
-        {/* Meta para robots */}
-        <meta name="robots" content="index, follow" />
-
-        {/* Etiquetas meta para SEO */}
-        <meta name="description" content={description} />
-        <meta name="keywords" content={keywords} />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content="https://sempen.com/images/imagen.jpg" />
-
-        {/* Etiqueta Canonical */}
-        <link rel="canonical" href={canonicalUrl} />
-
-        {/* Título de la página */}
-        <title>{metadata.title}</title>
-      </Head>
-      <body className={archivo.className}>
-        {children}
-      </body>
+      <body className={archivo.className}>{children}</body>
     </html>
   );
 }
