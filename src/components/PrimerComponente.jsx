6fee20fd36098ab508365a51536fe1b35fb51bfa
agustin mateos <agustinrotondomateos@gmail.com>
2024-12-06 20:26:57 -0300

    video

--- src/components/PrimerComponente.jsx
+++ src/components/PrimerComponente.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 
 export default function PrimerComponente({ shouldPlay }) {
   const [isMobile, setIsMobile] = useState(false);
+  const [videoSrc, setVideoSrc] = useState(null);
 
   useEffect(() => {
     if (!shouldPlay) return;
@@ -15,36 +16,34 @@ export default function PrimerComponente({ shouldPlay }) {
     checkDeviceSize();
     window.addEventListener('resize', checkDeviceSize);
 
+    // Carga el video en memoria al iniciar el componente
+    const videoUrl = isMobile ? '/Mobile.webm' : '/sempenDesktop.mp4';
+    fetch(videoUrl)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const localVideoUrl = URL.createObjectURL(blob);
+        setVideoSrc(localVideoUrl);
+      })
+      .catch((error) => console.error('Error loading video:', error));
+
     return () => {
       window.removeEventListener('resize', checkDeviceSize);
+      if (videoSrc) URL.revokeObjectURL(videoSrc); // Libera la memoria usada por el blob
     };
-  }, [shouldPlay]);
+  }, [isMobile, shouldPlay]);
 
   return (
     <div className="relative w-full overflow-hidden bg-[#16222F]">
-      {isMobile ? (
+      {videoSrc && (
         <video
-          id="videoSmall"
-          src="/Mobile.webm"
+          src={videoSrc}
           preload="auto"
           className="w-full h-full object-contain"
           loop
           muted
           autoPlay
           playsInline
-          onLoadedData={() => console.log('Mobile video loaded')}
-        />
-      ) : (
-        <video
-          id="videoLarge"
-          src="/sempenDesktop.mp4"
-          preload="auto"
-          className="w-full h-full object-contain"
-          loop
-          muted
-          autoPlay
-          playsInline
-          onLoadedData={() => console.log('Desktop video loaded')}
+          onLoadedData={() => console.log('Video loaded')}
         />
       )}
     </div>
