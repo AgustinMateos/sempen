8d78a0210d3dc7eab9f2421cad2404efc6931922
agustin mateos <agustinrotondomateos@gmail.com>
2024-12-07 19:28:19 -0300

    max-height: 100vh;

--- src/components/PrimerComponente.jsx
+++ src/components/PrimerComponente.jsx
@@ -64,3 +64,70 @@ export default function PrimerComponente({ shouldPlay }) {
     </div>
   );
 }
+
+// 'use client';
+
+// import { useEffect } from 'react';
+
+// export default function PrimerComponente({ shouldPlay }) {
+// useEffect(() => {
+// if (shouldPlay) {
+// const playVideo = () => {
+// const isDesktop = window.innerWidth > 640;
+// const largeVideo = document.getElementById('videoLarge');
+// const smallVideo = document.getElementById('videoSmall');
+
+// if (isDesktop) {
+// largeVideo?.play();
+// smallVideo?.pause();
+// } else {
+// smallVideo?.play();
+// largeVideo?.pause();
+// }
+// };
+
+// const handleResize = () => {
+// playVideo();
+// };
+
+// const handleVisibilityChange = () => {
+// if (!document.hidden) playVideo();
+// };
+
+// document.addEventListener('visibilitychange', handleVisibilityChange);
+// window.addEventListener('resize', handleResize);
+
+// playVideo();
+
+// return () => {
+// document.removeEventListener('visibilitychange', handleVisibilityChange);
+// window.removeEventListener('resize', handleResize);
+// };
+// }
+// }, [shouldPlay]);
+
+// return (
+// <div className="relative w-full h-[100vh] overflow-hidden">
+// {/* Video para pantallas grandes */}
+// <video
+// id="videoLarge"
+// src="/sempenDesktop.mp4"
+// preload="auto"
+// className="w-full h-[100%] object-fill hidden sm:block"
+// loop
+// muted
+// playsInline
+// />
+// {/* Video para pantallas pequeñas */}
+// <video
+// id="videoSmall"
+// src="/Mobile.webm"
+// preload="auto"
+// className="w-full h-[100%]  object-fill sm:hidden block"
+// loop
+// muted
+// playsInline
+// />
+// </div>
+// );
+// }
