55c5297935df1ca79345bca78162fb83ce82fdb2
agustin mateos <agustinrotondomateos@gmail.com>
2024-12-10 13:50:01 -0300

    cache

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,18 @@ const nextConfig = {
   async headers() {
     return [
       {
+        // Configuración para todas las páginas
+        source: '/:path*',
+        headers: [
+          { key: 'Cache-Control', value: 'public, max-age=0, must-revalidate' }, // Ajusta según tus necesidades
+        ],
+      },
+      {
+        // Configuración específica para archivos multimedia
         source: '/:path*\\.(webm|mp4|ogg)$',
         headers: [
-          { key: 'Cache-Control', value: 'public, max-age=31536000, immutable, ' },
-          { key: 'Accept-Ranges', value: 'bytes' }, 
+          { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
+          { key: 'Accept-Ranges', value: 'bytes' },
         ],
       },
     ];
