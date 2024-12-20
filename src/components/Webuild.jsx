d5c3b6bb1289e524d44f95053fa4c0d1d0162546
agustin mateos <agustinrotondomateos@gmail.com>
2024-12-20 17:41:57 -0300

    webuild

--- src/components/Webuild.jsx
+++ src/components/Webuild.jsx
@@ -38,11 +38,11 @@ export default function WeBuild() {
       <div className="absolute bottom-0 w-full flex justify-center">
         <div className="max-w-[1250px] rounded-[8px] w-full bg-[#16222F] h-[450px] md:h-[700px] lg:h-[450px] flex flex-col items-center justify-evenly px-4 mx-auto">
           <h4 className="font-archivo text-white text-[18px] md:text-[24px] mt-[20px] mb-2">{t('weArePartOf')} </h4>
-          <div className="flex justify-around md:space-x-4 flex-wrap h-[100%] w-[100%] items-center">
+          <div className="flex flex-col md:flex-row justify-around md:space-x-4 flex-wrap h-[100%] w-[100%] items-center">
             <Image src="/ahkBrasil_svg.svg" width={200} height={40} alt="ahk Logo" className="sm:w-[250px] sm:h-[12vh] md:w-[400px] md:h-[15vh] 2xl:h-[12vh]" />
             <Image src="/ahkUruguay_svg.svg" width={200} height={40} alt="ahk Logo" className="sm:w-[250px] sm:h-[12vh] md:w-[400px] md:h-[15vh] 2xl:h-[12vh]" />
             <Image src="/arg2.svg" width={200} height={40} alt="ahk Logo" className="sm:w-[250px] sm:h-[12vh] md:w-[400px] md:h-[15vh] 2xl:h-[12vh]" />           
-             <Image src="/rsb_svg.svg" width={200} height={50} alt="RSB Logo" className="w-[110px] h-[12vh] sm:w-[250px] sm:h-[12vh] md:w-[290px] md:h-[17vh] lg:w-[300px] lg:h-[20vh]   2xl:h-[12vh] " />
+             <Image src="/rsb_svg.svg" width={200} height={50} alt="RSB Logo" className="w-[160px] h-[12vh] sm:w-[250px] sm:h-[8vh] md:w-[290px] md:h-[17vh] lg:w-[300px] lg:h-[20vh]   2xl:h-[12vh] " />
             <Image src="/aviacaoSAF.png" width={200} height={50} alt="aviacao Saf Logo" className="w-[90px] h-[10vh] sm:w-[100px] sm:h-[12vh] md:w-[140px] md:h-[17vh] 2xl:h-[10vh]" />
           </div>
         </div>
