5d8f3ca2ef2d42691e17ea1dc71efd6bc7b2ae58
agustin mateos <agustinrotondomateos@gmail.com>
2024-12-20 14:48:51 -0300

    img we build

--- src/components/Webuild.jsx
+++ src/components/Webuild.jsx
@@ -23,7 +23,7 @@ export default function WeBuild() {
       }}
     >
       {/* Contenedor en fila */}
-      <div className="flex flex-col lg:flex-row items-center space-y-4 lg:space-y-0 lg:space-x-4 text-center lg:text-left px-4 lg:px-0 h-[500px] sm:h-[700px] md:h-[400px]  lg:h-[550px]">
+      <div className="flex flex-col lg:flex-row items-center space-y-4 lg:space-y-0 lg:space-x-4 text-center lg:text-left px-4 lg:px-0 h-[500px] sm:h-[700px] md:h-[600px]  lg:h-[550px]">
         <p className="text-[14px] sm:text-[18px] lg:text-[32px] font-light lg:leading-[108.8px] mb-[10px] sm:mb-[25px]">We build</p>
         <div className="relative mb-[25px]">
           <Image src="/Line-3.webp" width={550} height={34} alt="Line Image" className="w-[250px] lg:w-[420px]" />
@@ -36,13 +36,13 @@ export default function WeBuild() {
 
       {/* Contenedor azul oscuro con max-width */}
       <div className="absolute bottom-0 w-full flex justify-center">
-        <div className="max-w-[1050px] rounded-[8px] w-full bg-[#16222F] h-[450px] md:h-[500px] lg:h-[450px] flex flex-col items-center justify-evenly px-4 mx-auto">
+        <div className="max-w-[1050px] rounded-[8px] w-full bg-[#16222F] h-[450px] md:h-[700px] lg:h-[450px] flex flex-col items-center justify-evenly px-4 mx-auto">
           <h4 className="font-archivo text-white text-[18px] md:text-[24px] mt-[20px] mb-2">{t('weArePartOf')} </h4>
           <div className="flex justify-around md:space-x-4 flex-wrap h-[100%] w-[100%] items-center">
             <Image src="/ahkBrasil_svg.svg" width={200} height={40} alt="ahk Logo" className="sm:w-[250px] sm:h-[12vh] md:w-[400px] md:h-[15vh] 2xl:h-[12vh]" />
             <Image src="/ahkUruguay_svg.svg" width={200} height={40} alt="ahk Logo" className="sm:w-[250px] sm:h-[12vh] md:w-[400px] md:h-[15vh] 2xl:h-[12vh]" />
             <Image src="/arg2.svg" width={200} height={40} alt="ahk Logo" className="sm:w-[250px] sm:h-[12vh] md:w-[400px] md:h-[15vh] 2xl:h-[12vh]" />           
-             <Image src="/rsb_svg.svg" width={200} height={50} alt="RSB Logo" className="sm:w-[250px] sm:h-[12vh] md:w-[400px] md:h-[15vh] 2xl:h-[12vh]" />
+             <Image src="/rsb_svg.svg" width={200} height={50} alt="RSB Logo" className="w-[100px] h-[10vh] sm:w-[250px] sm:h-[12vh] md:w-[100px] md:h-[50px]   2xl:h-[12vh]" />
             <Image src="/aviacaoSAF.png" width={200} height={50} alt="aviacao Saf Logo" className="w-[100px] h-[12vh] sm:w-[100px] sm:h-[12vh] md:w-[140px] md:h-[17vh] 2xl:h-[10vh]" />
           </div>
         </div>
